5740612812fbdbd14a9c6ae50657497cb2873cc7
jonathan <jonathan@missioninspired.org>
2021-08-13 12:52:59 -0400

    schema

---
+++ messageSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const messageSchema = mongoose.Schema({
+    message: {type: String, required: true},
+    poster: {type: String, required: true},
+    recipients: {type: [String]},
+    prevMsg: {type: String},
+    nextMsg: {type: String},
+    dateCreated: {type: Date, default: new Date.now()}
+})
+
+const Message = mongoose.model('Message', messageSchema)
+module.exports = Message
